77123de04d259d56b3200d6005c0d69495b4939d
Andrea Stagi <stagi.andrea@gmail.com>
2014-08-05 16:36:54 +0200

    First import

---
+++ README.md
@@ -0,0 +1,7 @@
+Pebble-transparentimage
+=======================
+A convenient library to deal with transparent images on Pebble
+
+License
+-------
+MIT © 2014 Atooma Inc.
---
+++ transparentimage.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "pebble.h"
+
+typedef struct TransparentImageLayer {
+  BitmapLayer* white_layer;
+  BitmapLayer* black_layer;
+} TransparentImageLayer;
+
+typedef struct TransparentImage {
+  GBitmap *white_image;
+  GBitmap *black_image;
+  GRect bounds;
+} TransparentImage;
+
+TransparentImage* trasparent_image_create_with_resources(
+  uint32_t bitmap_black_res_id, uint32_t bitmap_white_res_id);
+
+TransparentImageLayer* transparent_image_layer_create(GRect image_frame,
+  const TransparentImage* transparent_image);
+
+Layer* transparent_image_layer_get_layer(const TransparentImageLayer* layer);
+
+void transparent_image_layer_destroy(TransparentImageLayer* layer);
+
+void transparent_image_destroy(TransparentImage* image);
